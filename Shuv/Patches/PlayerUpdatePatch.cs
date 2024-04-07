a3bd2640d962a25d25daf88744d7faf83f3eacf8
wwwDayDream <daymonforeman14@gmail.com>
2024-04-06 19:31:23 -0500

    Update to only allow pushing friends

--- Shuv/Patches/PlayerUpdatePatch.cs
+++ Shuv/Patches/PlayerUpdatePatch.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Collections.Generic;
 using System.Reflection;
 using BepInEx;
 using HarmonyLib;
+using Photon.Pun;
+using Steamworks;
 using UnityEngine;
 
 namespace Shuv.Patches;
@@ -11,6 +14,24 @@ public class PlayerUpdatePatch {
     private static float Charge { get; set; } = 0f;
     private const float ChargePower = 10f;
     private const float MaxFallTime = 1.5f;
+
+    private static List<CSteamID> Friends = new List<CSteamID>();
+    private static List<CSteamID> AlreadyChecked = new List<CSteamID>();
+
+    public static bool IsFriend(CSteamID steamID)
+    {
+        if (!AlreadyChecked.Contains(steamID))
+        {
+            if (SteamFriends.HasFriend(steamID, EFriendFlags.k_EFriendFlagImmediate))
+            {
+                Shuv.Logger.LogInfo("Is friends, can shove!");
+                Friends.Add(steamID);                
+            }
+            AlreadyChecked.Add(steamID);
+        }
+        return Friends.Contains(steamID);
+    }
+    
     [HarmonyPatch(nameof(Player.Update))]
     [HarmonyPostfix]
     private static void CheckForShuvBinding(Player __instance)
@@ -38,9 +59,15 @@ public class PlayerUpdatePatch {
                 var player = rayHit.collider.transform.parent.GetComponentInParent<Player>();
                 if (player != null)
                 {
-                    player.CallTakeDamageAndAddForceAndFall(0f, __instance.refs.cameraPos.forward * Charge * 
-                                                                (ChargePower / (player.ai ? 4f : 1f)), Charge * MaxFallTime + 0.5f);
-                    player.CallMakeSound(0);
+                    if (player.ai || (SteamAvatarHandler.TryGetSteamIDForPlayer(player.refs.view.Controller, out var steamID) && IsFriend(steamID)))
+                    {
+                        player.CallTakeDamageAndAddForceAndFall(0f, __instance.refs.cameraPos.forward * Charge * 
+                                                                    (ChargePower / (player.ai ? 4f : 1f)), Charge * MaxFallTime + 0.5f);
+                        player.CallMakeSound(0);
+                    } else
+                    {
+                        Shuv.Logger.LogInfo("Blocking Non-AI & Non-Friend Shuv!");
+                    }
                 }
             }
             Charge = 0f;
