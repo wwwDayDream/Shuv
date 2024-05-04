f5b9269c140602b2110fa8867ba9817615f50063
wwwDayDream <daymonforeman14@gmail.com>
2024-05-04 13:42:32 -0500

    Working refactor

--- Shuv/Patches/PlayerUpdatePatch.cs
+++ Shuv/Patches/PlayerUpdatePatch.cs
@@ -18,54 +18,34 @@ public class PlayerUpdatePatch {
     [HarmonyPostfix]
     private static void CheckForShuvBinding(Player __instance)
     {
-        if (!__instance.refs.view.IsMine) return;
-        if (__instance.data.health <= 0f || __instance.data.dead)
-        {
-            Charge = 0f;
-            return;
-        } // Don't allow shoving when dead, we use a '.health' check because the Defibrillator doesn't change '.dead' to false (iirc)
+        if (!__instance.refs.view.IsMine || __instance.refs == null) return;
+        var alive = __instance.data.health <= 0f || __instance.data.dead;
+        // Don't allow shoving when dead, we use a '.health' check because the Defibrillator doesn't change '.dead' to false (iirc)
         // If the Defibrillator does change '.dead' to false, feel free to remove the '.health' check
 
-        if (Shuv.ShuvKey.GetKey() && __instance.refs != null && __instance.refs.items != null)
+        if (Shuv.ShuvKey.GetKey() && __instance.refs.items != null && alive)
         {
             Charge = Mathf.MoveTowards(Charge, 1f, Time.deltaTime);
             if (Time.time > __instance.refs.items.shakeTime + 0.1f)
             {
                 GamefeelHandler.instance.perlin.AddShake(Charge, 0.2f, 15f);
                 __instance.refs.items.shakeTime = Time.time;
-                return;
             }
         }
-        if (!Shuv.ShuvKey.GetKey() && Charge > 0.25f)
+        if (!Shuv.ShuvKey.GetKey() && Charge > 0.25f && alive)
         {
             var ray = new Ray(__instance.refs.cameraPos.position, __instance.refs.cameraPos.forward);
+            // ReSharper disable once Unity.PreferNonAllocApi
             var hits = Physics.RaycastAll(ray, 2f, HelperFunctions.GetMask(HelperFunctions.LayerType.All));
-            if (hits == null)
-            {
-                Charge = 0f;
-                return;
-            }
             
-            Player? hitPlayer = null;
-            foreach (var raycastHit in hits)
-                if ((hitPlayer = raycastHit.collider.transform.parent.GetComponentInParent<Player>()) && !hitPlayer.IsLocal) // keep in mind, !hitPlayer.IsLocal isn't gonna work here; I tried '&& !(hitPlayer == __instance))' and that didn't work for some reason.
-                    break;
+            var hitPlayer = hits
+                .Select(raycastHit => raycastHit.collider.transform.parent.GetComponentInParent<Player>())
+                .FirstOrDefault(curHit => curHit && !curHit.IsLocal);
             
             Shuv.Logger.LogDebug("Trying shove...");
-            if (hitPlayer == null || !hitPlayer)
-            {
-                Charge = 0f;
-                return;
-            }
-            if (hitPlayer != null)
+            if (hitPlayer != null && hitPlayer)
             {
                 Shuv.Logger.LogDebug(hitPlayer.ai ? "Shoving AI" : "Shoving Player");
-                if (hitPlayer == __instance)
-                {
-                    Shuv.Logger.LogWarning("Just prevented self-shove!");
-                    Charge = 0f;
-                    return;
-                }
                 if (!hitPlayer.ai || ShuvConfig.ShoveEnemies)
                 {
                     hitPlayer.CallTakeDamageAndAddForceAndFall(ShuvConfig.Damage, 
@@ -74,8 +54,7 @@ public class PlayerUpdatePatch {
                     hitPlayer.CallMakeSound(0);
                 }
             }
-            Charge = 0f;
         }
-        if (!Shuv.ShuvKey.GetKey() && Charge > 0) Charge = 0f;
+        if ((!alive || !Shuv.ShuvKey.GetKey()) && Charge > 0) Charge = 0f;
     }
 }
